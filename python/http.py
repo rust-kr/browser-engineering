8ee51748284ce2cc53f104e3ceb7bed0106813c7
Dong-hee Na <donghee.na92@gmail.com>
2021-04-21 22:52:51 +0900

    http.py: use f-string for all cases

--- python/http.py
+++ python/http.py
@@ -9,8 +9,8 @@ except ImportError:
 
 def request(url):
     # 1. Parse scheme
-    scheme, url = url.split("://", 1)
-    assert scheme in ["http", "https"], "Unknown scheme {}".format(scheme)
+    scheme, url = url.split('://', 1)
+    assert scheme in ['http', 'https'], f'Unknown scheme {scheme}'
     port = 80 if scheme == "http" else 443
 
     # 2. Parse host
@@ -19,47 +19,47 @@ def request(url):
 
     # 3. Parse port
     if ":" in host:
-        host, port = host.split(":", 1)
+        host, port = host.split(':', 1)
         port = int(port)
 
     # 4. Connect
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    if scheme == "https":
+    if scheme == 'https':
         ctx = ssl.create_default_context()
         s = ctx.wrap_socket(s, server_hostname=host)
     s.connect((host, port))
 
     # 5. Send request
-    s.send("GET {} HTTP/1.0\r\n".format(path).encode("utf8"))
-    s.send("Host: {}\r\n".format(host).encode("utf8"))
-    s.send("Accept-Encoding: br,gzip,deflate\r\n".encode("utf8"))
-    s.send("\r\n".encode("utf8"))
+    s.send(f'GET {path} HTTP/1.0\r\n'.encode('utf8'))
+    s.send(f'Host: {host}\r\n'.encode('utf8'))
+    s.send('Accept-Encoding: br,gzip,deflate\r\n'.encode('utf8'))
+    s.send('\r\n'.encode('utf8'))
 
     # 6. Receive response
-    response = s.makefile("rb", newline="\r\n")
+    response = s.makefile('rb', newline='\r\n')
 
     # 7. Read status line
-    line = response.readline().decode('utf8')
+    line = response.readline().decode()
     # 8. Parse status line
     version, status, explanation = line.split(" ", 2)
 
     # 9. Check status
-    assert status == "200", "{}: {}".format(status, explanation)
+    assert status == "200", f'{status}: {explanation}'
 
     # 10. Parse headers
     headers = {}
     while True:
-        line = response.readline().decode('utf8')
-        if line == "\r\n":
+        line = response.readline().decode()
+        if line == '\r\n':
             break
-        header, value = line.split(":", 1)
+        header, value = line.split(':', 1)
         headers[header.lower()] = value.strip()
 
     body = response.read()
     if 'content-encoding' in headers:
         encoding = headers['content-encoding']
         body = decompress(body, encoding)
-    body = body.decode('utf8')
+    body = body.decode()
     s.close()
 
     # 12. Return
