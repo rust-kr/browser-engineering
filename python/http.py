7b1e32fd83146691ec2f783c025d9517bc5f7000
Dong-hee Na <donghee.na92@gmail.com>
2021-04-21 17:33:16 +0900

    ch1: Implement decompression for br/gzip/deflate

--- python/http.py
+++ python/http.py
@@ -1,6 +1,11 @@
+import gzip
 import socket
 import ssl
-
+import zlib
+try:
+    import brotli
+except ImportError:
+    brotli = None
 
 def request(url):
     # 1. Parse scheme
@@ -26,14 +31,15 @@ def request(url):
 
     # 5. Send request
     s.send("GET {} HTTP/1.0\r\n".format(path).encode("utf8"))
-    s.send("Host: {}\r\n\r\n".format(host).encode("utf8"))
+    s.send("Host: {}\r\n".format(host).encode("utf8"))
+    s.send("Accept-Encoding: br,gzip,deflate\r\n".encode("utf8"))
+    s.send("\r\n".encode("utf8"))
 
     # 6. Receive response
-    response = s.makefile("r", encoding="utf8", newline="\r\n")
+    response = s.makefile("rb", newline="\r\n")
 
     # 7. Read status line
-    line = response.readline()
-
+    line = response.readline().decode('utf8')
     # 8. Parse status line
     version, status, explanation = line.split(" ", 2)
 
@@ -43,18 +49,36 @@ def request(url):
     # 10. Parse headers
     headers = {}
     while True:
-        line = response.readline()
+        line = response.readline().decode('utf8')
         if line == "\r\n":
             break
         header, value = line.split(":", 1)
         headers[header.lower()] = value.strip()
 
-    # 11. Read body
     body = response.read()
+    if 'content-encoding' in headers:
+        encoding = headers['content-encoding']
+        body = decompress(body, encoding)
+    body = body.decode('utf8')
     s.close()
 
     # 12. Return
     return headers, body
+
+
+def decompress(data, encoding):
+    if encoding == 'gzip':
+        return gzip.decompress(data)
+    elif encoding == 'deflate':
+        return zlib.decompress(data, wbits=-zlib.MAX_WBITS)
+    elif encoding == 'br':
+        if brotli is None:
+            raise RuntimeError('please install brotli package: pip install brotli')
+        return brotli.decompress(data)
+    elif encoding == 'identity':
+        return data
+    else:
+        raise RuntimeError(f'unexpected content-encoding: {encoding}')
 
 
 def load(url):
