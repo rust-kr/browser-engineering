06cd10df561a79188c74d5829b8454c18f8f04cd
Dong-hee Na <donghee.na92@gmail.com>
2021-04-21 22:59:03 +0900

    http.py: clean up

--- python/http.py
+++ python/http.py
@@ -7,63 +7,64 @@ try:
 except ImportError:
     brotli = None
 
+
 def request(url):
     # 1. Parse scheme
     scheme, url = url.split('://', 1)
     assert scheme in ['http', 'https'], f'Unknown scheme {scheme}'
-    port = 80 if scheme == "http" else 443
+    port = 80 if scheme == 'http' else 443
 
     # 2. Parse host
-    host, path = url.split("/", 1)
-    path = "/" + path
+    host, path = url.split('/', 1)
+    path = '/' + path
 
     # 3. Parse port
-    if ":" in host:
+    if ':' in host:
         host, port = host.split(':', 1)
         port = int(port)
 
     # 4. Connect
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    if scheme == 'https':
-        ctx = ssl.create_default_context()
-        s = ctx.wrap_socket(s, server_hostname=host)
-    s.connect((host, port))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP) as s:
+        if scheme == 'https':
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=host)
 
-    # 5. Send request
-    s.send(f'GET {path} HTTP/1.0\r\n'.encode('utf8'))
-    s.send(f'Host: {host}\r\n'.encode('utf8'))
-    s.send('Accept-Encoding: br,gzip,deflate\r\n'.encode('utf8'))
-    s.send('\r\n'.encode('utf8'))
+        s.connect((host, port))
 
-    # 6. Receive response
-    response = s.makefile('rb', newline='\r\n')
+        # 5. Send request
+        s.send(f'GET {path} HTTP/1.0\r\n'.encode())
+        s.send(f'Host: {host}\r\n'.encode())
+        s.send('Accept-Encoding: br,gzip,deflate\r\n'.encode())
+        s.send('\r\n'.encode())
 
-    # 7. Read status line
-    line = response.readline().decode()
-    # 8. Parse status line
-    version, status, explanation = line.split(" ", 2)
+        # 6. Receive response
+        response = s.makefile('rb', newline='\r\n')
 
-    # 9. Check status
-    assert status == "200", f'{status}: {explanation}'
-
-    # 10. Parse headers
-    headers = {}
-    while True:
+        # 7. Read status line
         line = response.readline().decode()
-        if line == '\r\n':
-            break
-        header, value = line.split(':', 1)
-        headers[header.lower()] = value.strip()
+        # 8. Parse status line
+        version, status, explanation = line.split(' ', 2)
 
-    body = response.read()
-    if 'content-encoding' in headers:
-        encoding = headers['content-encoding']
-        body = decompress(body, encoding)
-    body = body.decode()
-    s.close()
+        # 9. Check status
+        assert status == "200", f'{status}: {explanation}'
 
-    # 12. Return
-    return headers, body
+        # 10. Parse headers
+        headers = {}
+        while True:
+            line = response.readline().decode()
+            if line == '\r\n':
+                break
+            header, value = line.split(':', 1)
+            headers[header.lower()] = value.strip()
+
+        body = response.read()
+        if 'content-encoding' in headers:
+            encoding = headers['content-encoding']
+            body = decompress(body, encoding)
+            body = body.decode()
+
+        # 12. Return
+        return headers, body
 
 
 def decompress(data, encoding):
@@ -79,12 +80,6 @@ def decompress(data, encoding):
         return data
     else:
         raise RuntimeError(f'unexpected content-encoding: {encoding}')
-
-
-def load(url):
-    # 14. Wire up
-    headers, body = request(url)
-    show(body)
 
 
 def lex(body):
