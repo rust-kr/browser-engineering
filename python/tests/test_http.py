a98392f2aab378d1abaaaab29ccef4c5fa8ac48d
Dong-hee Na <donghee.na92@gmail.com>
2021-04-22 19:01:27 +0900

    python: Support http/1.1

---
+++ python/tests/test_http.py
@@ -0,0 +1,32 @@
+import unittest
+
+from http import request
+
+
+class RequestTest(unittest.TestCase):
+    def test_http_request(self):
+        http_sites = ["http://www.google.com/", "http://example.com/"]
+        for site in http_sites:
+            headers, body = request(site)
+            self.assertGreater(len(body), 0)
+            self.assertIn("content-type", headers)
+
+    def test_https_request(self):
+        https_sites = [
+            "https://www.google.com/",
+            "https://www.facebook.com/",
+            "https://example.com/",
+        ]
+        for site in https_sites:
+            headers, body = request(site)
+            self.assertGreater(len(body), 0)
+            self.assertIn("content-type", headers)
+
+    def test_data_request(self):
+        headers, body = request("data:text/html,Hello world")
+        self.assertEqual(body, "Hello world")
+        self.assertEqual(headers["content-type"], "text/html")
+
+
+if __name__ == "__main__":
+    unittest.main()
